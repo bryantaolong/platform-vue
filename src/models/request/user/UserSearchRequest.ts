26df444898dce56c783bb37331d41057338c4e69
Bryan Long <bryantaolong@outlook.com>
2025-07-31 10:41:46 +0800

    Add UserSearchRequest model

---
+++ src/models/request/user/UserSearchRequest.ts
@@ -0,0 +1,35 @@
+/**
+ * @module src/models/request/user/UserSearchRequest.ts
+ * @description 定义搜索用户请求的数据结构。
+ */
+export interface UserSearchRequest {
+    username: string;
+
+    phoneNumber: string;
+
+    email: string;
+
+    gender: string;
+
+    status: string;
+
+    roles: string;
+
+    loginTime: string;
+
+    loginIp: string;
+
+    passwordResetTime: string;
+
+    createTime: string;
+
+    createTimeStart: string;
+
+    createTimeEnd: string;
+
+    updateTime: string;
+
+    updateTimeStart: string;
+
+    updateTimeEnd: string;
+}
