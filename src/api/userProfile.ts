b64efdcf6b487bb10b29b16a12e8d5c7f034f014
Bryan Long <bryantaolong@outlook.com>
2025-08-27 09:09:49 +0800

    Remove useless MyBatisPageResult wrapper within userProfile.ts

--- src/api/userProfile.ts
+++ src/api/userProfile.ts
@@ -1,7 +1,6 @@
 // src/api/userProfile.ts
 import request from '@/utils/request';
 import type { Result } from '@/models/response/Result';
-import type { MyBatisPageResult } from '@/models/response/MyBatisPageResult.ts';
 import type {UserProfileVO} from "@/models/vo/UserProfileVO.ts";
 import type {UserProfile} from "@/models/entity/UserProfile.ts";
 import type {UserUpdateRequest} from "@/models/request/user/UserUpdateRequest.ts"; // Re-using MongoPageResult from user API
@@ -24,7 +23,7 @@ export function unfollowUser(realName: string): Promise<Result<UserProfileVO>> {
     });
 }
 
-export function getCurrentUserProfile(): Promise<Result<MyBatisPageResult<UserProfileVO>>> {
+export function getCurrentUserProfile(): Promise<Result<UserProfileVO>> {
     return request({
         url: `/api/user-profiles/me`,
         method: 'get',
@@ -34,7 +33,7 @@ export function getCurrentUserProfile(): Promise<Result<MyBatisPageResult<UserPr
 export function updateUser(
     userId: number,
     req: UserUpdateRequest
-): Promise<Result<MyBatisPageResult<UserProfile>>> {
+): Promise<Result<UserProfile>> {
     return request({
         url: `/api/user-profiles/${userId}`,
         method: 'get',
